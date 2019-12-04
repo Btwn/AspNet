ba77a2ffed792eb35510ba4c419b2c58677f9d77
Jair D. Rivera Virgen <jdrivera@grupomavi.com>
2019-12-03 18:47:28 -0600

    se agrega aplicacion capitulo 2

---
+++ capitulo2/WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Linq;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good Morning" : "good afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+
+                return View("Thnkx", guestResponse);
+            }
+            else 
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
