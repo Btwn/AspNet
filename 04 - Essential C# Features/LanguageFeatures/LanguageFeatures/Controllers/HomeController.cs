e9b70f8564bffb53df1ffc83b3d1e040e0e7fef2
Carlos A. Diaz <carlos4diazj@hotmail.com>
2019-12-09 01:00:00 -0600

    practica capitulo 4

---
+++ 04 - Essential C# Features/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            Product[] productArray = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            decimal cartTotal = cart.TotalPrices();
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+            return View("Index", new string[] { $"Cart Total: {cartTotal:C2}", $"Array Total: {arrayTotal:C2}" });
+
+            //////////////////////////////////////////////////////////////
+            //object[] data = new object[] { 275M, 29.95M,"apple", "orange", 100, 10 };
+            //decimal total = 0;
+            //for (int i = 0; i < data.Length; i++)
+            //{
+            //    switch (data[i])
+            //    {
+            //        case decimal decimalValue:
+            //            total += decimalValue;
+            //            break;
+            //        case int intValue when intValue > 50:
+            //            total += intValue;
+            //            break;
+            //    }
+            //}
+            //return View("Index", new string[] { $"Total: {total:C2}" });
+
+            /////////////////////////////////////////////////////
+            //Dictionary<string, Product> products = new Dictionary<string, Product>
+            //{
+            //    [ "Kayak" ] = new Product { Name = "Kayak", Category = "Water Craft", Price = 275M },
+            //    [ "Lifejacket" ] = new Product { Name = "Lifejacket", Price = 48.95M }
+            //};
+            //return View("Index", products.Keys);
+
+            ///////////////////////////////////////////////
+            //List<string> results = new List<string>();
+
+            //foreach (Product p in Product.GetProducts())
+            //{
+            //    string name = p?.Name ?? "<No Name>";
+            //    string category = p?.Category;
+            //    decimal? price = p?.Price ?? 0;
+            //    string relatedName = p?.Related?.Name ?? "<None>";
+            //    results.Add(string.Format($"Name: {name}, Category: {category}, Price: {price:C2}, Related: {relatedName}"));
+            //}
+
+            //return View(results);
+
+            /////////////////////////////////////////////
+            //return View(new string[] { "C#", "language", "Features" });
+        }
+    }
+}
