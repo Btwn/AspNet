db4a98b8fe4d98452f28f9bb0f557c0490b56bc2
David Dado Sandoval <da.dado@mavi.mx>
2019-12-09 12:04:46 -0600

    proyecto 05Razor

---
+++ 04LanguajeFeatures/04LanguajeFeatures/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _04LanguajeFeatures.Models;
+
+namespace _04LanguajeFeatures.Controllers
+{
+    public class HomeController: Controller
+    {
+        //public ViewResult Index()
+        //{
+        //    List<string> results = new List<string>();
+        //    foreach (Product p in Product.GetProducts())
+        //    {
+        //        string name = p?.Name ?? "<No Name>";
+        //        decimal? price = p?.Price ?? 0;
+        //        string relatedName = p?.Related?.Name ?? "<none>";
+        //        string category = p?.Category ?? "<none>";
+        //        //results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+        //        results.Add($"Name: {name}, Price: {price}, Related: {relatedName}, Category: {category}");
+        //    }
+        //    return View(results);
+        //}
+
+        //       public ViewResult Index()
+        //       {
+        //           object[] data = new object[] { 275M, 29.95M,
+        //"apple", "orange", 100, 10 };
+        //           decimal total = 0;
+        //           for (int i = 0; i < data.Length; i++)
+        //           {
+        //               switch (data[i])
+        //               {
+        //                   case decimal decimalValue:
+        //                       total += decimalValue;
+        //                       break;
+        //                   case int intValue when intValue > 50:
+        //                       total += intValue;
+        //                       break;
+        //               }
+        //           }
+        //           return View("Index", new string[] { $"Total: {total:C2}" });
+        //       }
+        public ViewResult Index()
+        {
+            return View(Product.GetProducts().Select(p => p?.Name));
+        }
+    }
+}
