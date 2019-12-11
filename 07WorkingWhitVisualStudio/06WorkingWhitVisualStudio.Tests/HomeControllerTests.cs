ec982b120bf62a61ca283473d712f648839b0255
David Dado Sandoval <da.dado@mavi.mx>
2019-12-11 10:22:42 -0600

    practica 06

---
+++ 07WorkingWhitVisualStudio/06WorkingWhitVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using _06WorkingWhitVisualStudio.Controllers;
+using _06WorkingWhitVisualStudio.Models;
+using Xunit;
+
+
+namespace _06WorkingWhitVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModellsComplete()
+        {
+            //Arrange
+            var controller = new HomeController();
+            
+
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            
+            //Assert
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+    }
+}
---
+++ 07WorkingWhitVisualStudio/06WorkingWhitVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using _06WorkingWhitVisualStudio.Models;
+
+namespace _06WorkingWhitVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            //Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            //Act
+            p.Name = "New Name";
+
+            //Assert
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            //Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Price = 200;
+
+            //Assert
+            Assert.Equal(200M, p.Price);
+        }
+    }
+}
