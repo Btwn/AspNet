22559859dc98c4886ee8e6a54db0986749cfc98b
Carlos A. Diaz <carlos4diazj@hotmail.com>
2019-12-12 18:57:58 -0600

    Antes de usar bootstrap

---
+++ 08-12 - SportsStore/SportsStore/SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+//  dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 3.0.0
+//  dotnet add package Microsoft.EntityFrameworkCore.Design
+
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- 08-12 - SportsStore/SportsStore/SportsStore/Startup.cs
+++ 08-12 - SportsStore/SportsStore/SportsStore/Startup.cs
@@ -8,16 +8,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
 
@@ -40,12 +48,22 @@ namespace SportsStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default", "{controller=Product}/{action=List}");
+                endpoints.MapControllerRoute(
+                    name: "pagination",
+                    pattern: "Products/Page{productPage}",
+                    //pattern: "{controller=Product}/{action=List}/{productPage}",
+                    //pattern: "Page{productPage}",
+                    new { Controller = "Product", Action = "List" });
+                
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Product}/{action=List}/{id?}");
                 //endpoints.MapGet("/", async context =>
                 //{
                 //    await context.Response.WriteAsync("Hello World!");
                 //});
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
