dc93a2a118d8d61939a4dc0c342806cb2f7a8e07
Carlos A. Diaz <carlos4diazj@hotmail.com>
2019-12-20 19:29:20 -0600

    capitulo 15

---
+++ 15 - URL Routing/UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Result", new Result { 
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+
+        public ViewResult CustomVariable(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(Index)
+            };
+            //r.Data["Id"] = RouteData.Values["id"];
+            r.Data["Id"] = id ?? "<no value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
+    }
+}
