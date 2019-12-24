cf949326afb0aaa75d47204b6bf67c3e0f01ac09
abgonzalez <6ckgwmh41>
2019-12-23 18:45:40 -0600

    ValidacioDeFormularios

---
+++ proyecto13/Proyecto13/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto13.Models;
+
+namespace Proyecto13.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Persona per = new Persona();
+            return View(per);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Persona per)
+        {
+            if (ModelState.IsValid)
+                return View("Correcto");
+            else
+                return View(per);
+        }
+    }
+}
