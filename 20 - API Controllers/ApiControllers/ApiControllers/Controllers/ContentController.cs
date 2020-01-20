20a4a0b50f91a36f0eca0fbe229f73657398ea2d
Carlos A. Diaz <carlos4diazj@hotmail.com>
2020-01-20 10:20:17 -0600

    ApiControllers capitulo 20

---
+++ 20 - API Controllers/ApiControllers/ApiControllers/Controllers/ContentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiControllers.Models;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller
+    {
+        [HttpGet("string")]
+        //[Produces("application/json")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public Reservation GetObject() => new Reservation
+        {
+            ReservationId = 1000,
+            ClientName = "Joe",
+            Location = "Board Room"
+        };
+    }
+}
