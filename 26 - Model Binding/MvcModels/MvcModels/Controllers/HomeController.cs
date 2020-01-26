9b25764927519c3df492b6c48c14a97a896d315d
Carlos A. Diaz <carlos4diazj@hotmail.com>
2020-01-25 18:32:45 -0600

    Model Binding capitulo 26

---
+++ 26 - Model Binding/MvcModels/MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcModels.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        //public ViewResult Index() => View(repository);
+
+        public IActionResult Index([FromQuery] int? id)
+        {
+            Person person;
+            if (id.HasValue && (person = repository[id.Value]) != null)
+            {
+                return View(person);
+            }
+            else
+            {
+                return NotFound();
+            }
+            //View(repository[id] ?? repository.People.First());
+        }
+
+        public ViewResult Create() => View(new Person());
+
+        [HttpPost]
+        public ViewResult Create(Person model) => View("Index", model);
+
+        public ViewResult DisplaySummary(
+            //[Bind(Prefix =nameof(Person.HomeAdress))] AddressSummary summary) => 
+            [Bind(nameof(AddressSummary.City), Prefix = nameof(Person.HomeAdress))] AddressSummary summary ) =>
+            View(summary);
+
+        //public ViewResult Names(string[] names) => View(names ?? new string[0]);
+        public ViewResult Names(IList<string> names) => View(names ?? new List<string>());
+
+        public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
+
+        //public string Header([FromHeader] string accept) => $"Header: {accept}";
+        //public string Header([FromHeader(Name = "Accept-Language")] string accept) => $"Header: {accept}";
+        public ViewResult Header(HeaderModel model) => View(model);
+
+        public ViewResult Body() => View();
+
+        [HttpPost]
+        public Person Body([FromBody] Person model) => model;
+    }
+}
---
+++ 26 - Model Binding/MvcModels/MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MvcModels.Models
+{
+    //[Bind(nameof(Country))]
+    public class AddressSummary
+    {
+        public string City { get; set; }
+
+        //[BindNever]
+        public string Country { get; set; }
+    }
+}
