5a194179d6eda422800ee6d1635d811006d0517a
Carlos A. Diaz <carlos4diazj@hotmail.com>
2020-01-28 15:00:54 -0600

    capitulo 28

---
+++ 28 - Getting Started with Identity/Users/Users/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Users.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Users.Infrastructure
+{
+    public class CustomUserValidator : UserValidator<AppUser>
+    {
+        public override async Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            IdentityResult result = await base.ValidateAsync(manager, user);
+
+            List<IdentityError> errors = result.Succeeded ? new List<IdentityError>() : result.Errors.ToList();
+            
+            if (!user.Email.ToLower().EndsWith("@example.com"))
+            {
+                errors.Add(new IdentityError {
+                    Code = "EmailDomainError",
+                    Description = "Only example.com email address are allowed"
+                });
+            }
+
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
+    }
+}
